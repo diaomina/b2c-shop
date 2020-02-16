d1a6b587aed79b5add253f53c7617de9a06f113f
daiomina <diaomina@qq.com>
2020-02-16 21:35:42 +0800

    chore 项目更新为多环境配置

---
+++ src/test/resources/static/shop-style/js/vdu.js
@@ -0,0 +1,3 @@
+﻿
+/* hdpic */
+$(document).ready(function(){$(".hdpic dd a").hover(function(){$(this).find(".picshine").stop();$(this).find(".picshine").css("background-position","-235px 0");$(this).find(".picshine").animate({backgroundPosition: '235px 0'},500);$(this).find(".pictitle").stop().animate({left:'0px'},{queue:false,duration:450});},function(){$(this).find(".pictitle").stop().animate({left:'-235px'},{queue:false,duration:200});});});
